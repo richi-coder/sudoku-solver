a3fe145e7d3f1ecbf0a1fb79c47e7068c2318745
richi-coder <richicoder@gmail.com>
2023-05-31 17:43:16 -0400

    adding localStorage to persist data

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -2,7 +2,14 @@ import { useState } from "react"
 import Cell from "./Cell"
 
 function Dashboard() {
-    const [dashboard, setDashboard] = useState(new Array(9).fill(new Array(9).fill('')))
+    const [dashboard, setDashboard] = useState(() => {
+      try {
+        const initDashboard = window.localStorage.getItem('dashboard');
+        return initDashboard ? JSON.parse(initDashboard) : new Array(9).fill(new Array(9).fill(''))
+      } catch (error) {
+        return new Array(9).fill(new Array(9).fill(''));
+      }
+    })
 
     const userSudoku = (position: any, cell: string):void => {
         const { i, j} = position;
@@ -10,18 +17,22 @@ function Dashboard() {
         const iLine = copy[i].map((square: string, index: number) => index === j ? cell : square)
         copy[i] = iLine;
         const updatedDashboard = [...copy]
-        console.log(updatedDashboard, 'updated');
+        window.localStorage.setItem('dashboard', JSON.stringify(updatedDashboard))        
         setDashboard(updatedDashboard)
     }
 
   return (
-    <div className='grid grid-cols-9'>
-        {
-            dashboard.map((line, i: number) => 
-                line.map((cell: string, j: number) => 
-                        <Cell key={String(i) + String(j)} position={{i, j}} cell={cell} sudokuPuzzle={userSudoku} />
-            ))
-        }
+    <div className='flex flex-row'>
+      <div className='grid grid-cols-9'>
+          {
+              dashboard.map((line: [], i: number) => 
+                  line.map((cell: string, j: number) => 
+                          <Cell key={String(i) + String(j)} position={{i, j}} cell={cell} sudokuPuzzle={userSudoku} />
+              ))
+          }
+          
+      </div>
+      <button className='bg-blue-700 text-white'>solve</button>
     </div>
   )
 }
